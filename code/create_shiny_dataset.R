76e0d602699e6146fd00c6157ab577ec25d06f8c
Nico RIedel <nico.riedel@bihealth.de>
2021-05-04 19:26:20 +0200

    Rename old combination script to Shiny script
    Script not yet functional!

--- code/create_shiny_dataset.R
+++ code/create_shiny_dataset.R
@@ -1,89 +1,5 @@
 library(tidyverse)
-library(lubridate)
 
-#read in dataset
-IntoValue_dataset <- read_csv("data/IntoValue2_Dataset.csv", na = "NA")
-IntoValue1_dataset <- readRDS("shiny_app/data/CT_gov_2.rds")
-
-#----------------------------------------------------------------------------------------------------------------------
-# Loading of AACT dataset
-#----------------------------------------------------------------------------------------------------------------------
-
-#the AACT dataset has to be downloaded first from https://aact.ctti-clinicaltrials.org/pipe_files
-AACT_folder <- "C:/Datenablage/AACT/AACT dataset 20200603/" #insert the AACT download folder here
-
-#AACT filenames that we need to load
-AACT_dataset_names <- c("studies", "overall_officials", "sponsors", "responsible_parties",
-                        "facilities", "interventions", "calculated_values")
-
-AACT_dataset_files <- paste0(AACT_folder, AACT_dataset_names, ".txt")
-AACT_datasets <- AACT_dataset_files %>%
-  map(read_delim, delim = "|")
-names(AACT_datasets) <- AACT_dataset_names
-
-
-#----------------------------------------------------------------------------------------------------------------------
-# add the missing primary completion date to the IV2 dataset
-#----------------------------------------------------------------------------------------------------------------------
-
-PCD_join <- AACT_datasets$studies %>% 
-  select(nct_id, primary_completion_date, study_first_submitted_date) %>% 
-  rename(id = nct_id)
-
-IntoValue_dataset <- read_csv("data/IntoValue2_Dataset.csv", na = "NA")
-IntoValue_dataset <- IntoValue_dataset %>% 
-  left_join(PCD_join) %>%
-  mutate(primary_completion_year = primary_completion_date %>% str_sub(1,4),
-         primary_completion_date = primary_completion_date %>% ymd(),
-         study_first_submitted_date = study_first_submitted_date %>% ymd(),
-         `Days to summary PCD` = summary_res_date - primary_completion_date,
-         `Days to publication PCD` = publication_date - primary_completion_date,
-         days_PCD_to_reg = primary_completion_date - study_first_submitted_date)
-
-
-#----------------------------------------------------------------------------------------------------------------------
-# rename and modify IV2 variables to match the IV1 dataset format
-#----------------------------------------------------------------------------------------------------------------------
-
-#unify variables 
-id_step_name <- function(id_step_num)
-{
-  step_name = NA
-  if(id_step_num == 0) {
-    step_name = "No publ"
-  } else if(id_step_num == 1) {
-    step_name = "Registry linked"
-  } else if(id_step_num == 2) {
-    step_name = "Google ID search"
-  } else if(id_step_num == 3) {
-    step_name = "Hand search"
-  } else if(id_step_num == 8) {
-    step_name = "No publ"
-  }
-  
-  return(step_name)
-}
-
-IntoValue_dataset <- IntoValue_dataset %>% 
-  rename(`Days to publication CD` = days_to_publication,
-         `Days to summary CD` = days_to_summary,
-         `Summary results` = has_summary_results,
-         Publications = has_publication,
-         recruitmentStatus = overall_status,
-         days_CD_to_reg = days_reg_to_compl,
-         days_start_to_reg = days_reg_to_start,
-         days_publ_to_reg = days_reg_to_publ) %>%
-  mutate(is_CTgov = is_CTgov %>% map_chr(function(x) ifelse(x, "yes", "no")),
-         lead_or_facility = "lead",
-         publication_PMID = NA)
-
-IntoValue_dataset$`Publication type` = IntoValue_dataset$indentification_step %>% map_chr(id_step_name)
-IntoValue_dataset$intervention_type <- IntoValue_dataset$intervention_type %>% replace_na("Not given")
-
-#remove additional IV2 rows for now  
-IntoValue_dataset <- IntoValue_dataset %>% 
-  select(-indentification_step, -study_first_submitted_date,
-         -summary_res_date, -start_date)
 
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -121,6 +37,8 @@ common_variables
 variables_only_IV2
 variables_only_IV1
 
+#load combined dataset
+#....
 
 #unify city names
 IntoValue_dataset_trials_per_city$city[IntoValue_dataset_trials_per_city$city == "TU"] <- "TU München"
