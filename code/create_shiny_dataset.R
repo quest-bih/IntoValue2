35e53d82e0d0be98a5ea1bfb8979a8db05129283
Nico RIedel <nico.riedel@bihealth.de>
2021-05-20 12:59:30 +0200

    Adapts script to create shiny dataset to new combined dataset

--- code/create_shiny_dataset.R
+++ code/create_shiny_dataset.R
@@ -1,84 +1,67 @@
 library(tidyverse)
-
-
-
-#----------------------------------------------------------------------------------------------------------------------
-# bring IV2 dataset in the same shape as the IV1 dataset used for the shiny app
-# with one row for each combination of trial & UMC for easy filtering
-#----------------------------------------------------------------------------------------------------------------------
-
-#spread out IV2 dataset to one row for each trial + participating lead city
-cities <- IntoValue_dataset$lead_cities %>% str_split(" ") %>% unlist() %>% unique() %>% sort()
-#calculate list of logical vectors for each city name, assinging studies to city name
-cities_in_studies <- lapply(cities, grepl, x = IntoValue_dataset$lead_cities)
-names(cities_in_studies) <- cities
-cities_in_studies <- do.call(cbind, cities_in_studies)
-#insert NA for cases where we dont have a hit for later gathering into one column to make dataset tidy
-cities_in_studies[cities_in_studies == FALSE] <- NA
-
-#add the new colums to the dataset, allowing easy subsetting of the data
-IntoValue_dataset_trials_per_city <- as_tibble(cbind(IntoValue_dataset, cities_in_studies))%>%
-  gather(cities, key = "city", value = "City_NCT_Combination", na.rm = TRUE) %>%
-  select(-City_NCT_Combination) %>% 
-  arrange(id)
-
-#add a row for each trial with the "All trials combined" tag
-IntoValue_dataset_all_trials <- IntoValue_dataset %>%
-  mutate(city = "All trials combined")
-IntoValue_dataset_trials_per_city <- IntoValue_dataset_trials_per_city %>%
-  rbind(IntoValue_dataset_all_trials) %>%
-  select(-lead_cities)
-
-#check for remaning differences in variable names
-common_variables <- colnames(IntoValue_dataset_trials_per_city)[(colnames(IntoValue_dataset_trials_per_city) %in% colnames(IntoValue1_dataset))]
-variables_only_IV2 <- colnames(IntoValue_dataset_trials_per_city)[!(colnames(IntoValue_dataset_trials_per_city) %in% colnames(IntoValue1_dataset))]
-variables_only_IV1 <- colnames(IntoValue1_dataset)[!(colnames(IntoValue1_dataset) %in% colnames(IntoValue_dataset_trials_per_city))]
-common_variables
-variables_only_IV2
-variables_only_IV1
+library(assertthat)
 
 #load combined dataset
-#....
+iv_dataset <- read_csv("data/iv_main_dataset.csv")
+
+
+#----------------------------------------------------------------------------------------------------------------------
+# make one row per trial & UMC combination for easy subsetting
+#----------------------------------------------------------------------------------------------------------------------
+
+#all used city names
+cities <- iv_dataset$lead_cities %>% str_split(" ") %>% unlist() %>% unique() %>% sort()
+
+#function to spread table to one row per trial & UMC combination
+#has to be called separately for lead and facility
+make_trial_UMC_table <- function(iv_data, cities, city_col, category)
+{
+  #calculate list of logical vectors for each city name, assigning studies to city name
+  cities_in_studies <- lapply(cities, grepl, x = iv_data[[city_col]])
+  names(cities_in_studies) <- cities
+  cities_in_studies <- do.call(cbind, cities_in_studies)
+  #insert NA for cases where we dont have a hit for later gathering into one column to make dataset tidy
+  cities_in_studies[cities_in_studies == FALSE] <- NA
+  
+  #add the new colums to the dataset, allowing easy subsetting of the data
+  iv_data_trials_per_city <- as_tibble(cbind(iv_data, cities_in_studies)) %>%
+    gather(all_of(cities), key = "city", value = "City_NCT_Combination", na.rm = TRUE) %>%
+    mutate(lead_or_facility = category) %>%
+    select(-City_NCT_Combination)  %>% 
+    arrange(id)
+  
+  return(iv_data_trials_per_city)
+}
+
+#create one table for all lead trial & for all facility trials (IV1 only), then combine
+iv_trials_per_lead_city <- make_trial_UMC_table(iv_dataset, cities, "lead_cities", "lead")
+iv_trials_per_facility_city <- make_trial_UMC_table(iv_dataset, cities, "facility_cities", "facility")
+iv_trials_per_city <- rbind(iv_trials_per_lead_city, iv_trials_per_facility_city)
+
+#check for trials missing in the modified dataset
+missing_trials <- iv_dataset[!(iv_dataset$id %in% iv_trials_per_city$id ),]
+assert_that(dim(missing_trials)[1] == 0)
+
+
+#check that all cities have the right number of entries in the modified dataset
+assert_that(sum(city_counts_lead) ==  sum(id_occurences_lead))
+iv_lead_city_table <- iv_dataset$lead_cities %>% str_split(" ") %>% unlist() %>% table()
+iv_lead_trial_per_city_table <- iv_trials_per_lead_city$city %>% table()
+assert_that(all(iv_lead_city_table == iv_lead_trial_per_city_table))
+
+
+#add a row for each trial with the "All trials combined" tag
+iv_dataset_all_trials <- iv_dataset %>%
+  mutate(city = "All trials combined",
+         lead_or_facility = ifelse(is.na(lead_cities), "facility", "lead"))
+iv_trials_per_city <- iv_trials_per_city %>%
+  rbind(iv_dataset_all_trials)
+
 
 #unify city names
-IntoValue_dataset_trials_per_city$city[IntoValue_dataset_trials_per_city$city == "TU"] <- "TU München"
-IntoValue_dataset_trials_per_city$city[IntoValue_dataset_trials_per_city$city == "LMU"] <- "LMU München"
-IntoValue_dataset_trials_per_city$city[IntoValue_dataset_trials_per_city$city == "Charite"] <- "Berlin"
-
-#also combine Kiel and Lübeck from IV1
-IntoValue1_dataset$city[IntoValue1_dataset$city == "Kiel"] <- "Schleswig-Holstein"
-IntoValue1_dataset$city[IntoValue1_dataset$city == "Lübeck"] <- "Schleswig-Holstein"
-
-#need to remove duplicate cases that were counted for Kiel and Lübeck before
-IntoValue1_dataset <- IntoValue1_dataset %>% distinct(id, city, .keep_all = TRUE)
-
-
-#check for remaning differences in city names
-IV1_cities <- IntoValue1_dataset$city %>% unique() %>% sort()
-IV2_cities <- IntoValue_dataset_trials_per_city$city %>% unique() %>% sort()
-cities_only_IV1 <- IV1_cities[!(IV1_cities %in% IV2_cities)]
-cities_only_IV2 <- IV2_cities[!(IV2_cities %in% IV1_cities)]
-cities_only_IV1
-cities_only_IV2
-
-
-#combine datasets
-IntoValue1_dataset <- IntoValue1_dataset %>% 
-  mutate(IV_version = "IV1")
-IntoValue_dataset_trials_per_city <- IntoValue_dataset_trials_per_city %>% 
-  mutate(IV_version = "IV2")
-
-#remove duplicate trials in IV1 dataset before joining! - for now we remove the results from IV1 in those cases
-dupl_trials_IV1 <- IntoValue1_dataset[(IntoValue1_dataset$id %in% IntoValue_dataset$id),]$id
-dupl_trials_IV2 <- IntoValue_dataset_trials_per_city[(IntoValue_dataset_trials_per_city$id %in% IntoValue1_dataset$id),]$id
-
-IntoValue1_dataset[IntoValue1_dataset$id %in% dupl_trials_IV1,]$IV_version <- "IV1_dupl"
-IntoValue_dataset_trials_per_city[IntoValue_dataset_trials_per_city$id %in% dupl_trials_IV2,]$IV_version <- "IV2_dupl"
+iv_trials_per_city$city[iv_trials_per_city$city == "TU_München"] <- "TU München"
+iv_trials_per_city$city[iv_trials_per_city$city == "LMU_München"] <- "LMU München"
 
 
 
-IntoValue_dataset_comb <- rbind(IntoValue_dataset_trials_per_city, IntoValue1_dataset)  %>%
-  mutate(completion_year = str_sub(completion_date, 1, 4))
-
-
-write_rds(IntoValue_dataset_comb, "shiny_app/data/IntoValue_Dataset_combined.rds")
+write_rds(iv_trials_per_city, "shiny_app/data/IntoValue_Dataset_combined.rds")
